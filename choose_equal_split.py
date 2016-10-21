f90b9e694547baee584f70288b5417180c89fc98
x0s <guillaume.fidanza@gmail.com>
2016-10-21 12:15:43 +0200

    Split dataset refactorization in pure python

--- choose_equal_split.py
+++ choose_equal_split.py
@@ -1,32 +1,24 @@
-file = open('trainingData.csv')
+"""split data into training and validation sets"""
+import csv
 
-data = file.readlines()[1:]
-langs = set()
-for line in data:
-    filepath, language = line.split(',')
-    filepath = filepath.strip()
-    language = language.strip()
-    langs.add(language)
+with open('trainingData.csv', 'rb') as csvfile:
+    next(csvfile) #skip headers
+    data = list(csv.reader(csvfile, delimiter=','))
 
-langs = sorted(langs)
-ID = {}
-for id, lang in enumerate(langs):
-    ID[lang] = id
-file.close()
+    #Map every language to an ID
+    langs = set([language.strip() for _,language in data])
+    ID = {lang: i for i,lang in enumerate(sorted(langs))}
 
-train = open('trainEqual.csv', 'w')
-val = open('valEqaul.csv', 'w')
-cnt = [0 for x in range(176)]
-for line in data:
-    filepath, language = line.split(',')
-    filepath = filepath.strip()
-    language = language.strip()
-    id = ID[language]
-    if (cnt[id] < 306):
-        train.write(filepath[:-4] + ',' + str(ID[language]) + '\n')
-    else:
-        val.write(filepath[:-4] + ',' + str(ID[language]) + '\n')
-    cnt[id] += 1
-    
-train.close()
-val.close()
+    #Write first 306 items to training set and the rest to validation set
+    cnt = [0 for _ in range(len(langs))]
+    with open('trainEqual.csv', 'w') as train:
+        with open('valEqaul.csv', 'w') as val:
+            for line in data:
+                filepath, language = map(str.strip, line)
+                id_lang = ID[language]
+
+                if (cnt[id_lang] < 306):
+                    train.write(filepath[:-4] + ',' + str(id_lang) + '\n')
+                else:
+                    val.write(filepath[:-4] + ',' + str(id_lang) + '\n')
+                cnt[id_lang] += 1
